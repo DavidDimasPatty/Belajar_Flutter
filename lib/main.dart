3deb98e47c61963760211ea2800d6b9c2ba2a365
unknown <daviddimas80@gmail.com>
2022-07-09 23:59:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,31 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void printLog() {
+      print('Button Pressed');
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Aplikasi Gereja'),
+        ),
+        body: Column(
+          children: [
+            Text('Pilihan Menu'),
+            RaisedButton(
+              child: Text('Daftar Misa'),
+              onPressed: printLog,
+            ),
+            RaisedButton(
+              child: Text('Daftar Baptis'),
+              onPressed: () => print('Choosen'),
+            ),
+            RaisedButton(
+              child: Text('Daftar Komuni'),
+              onPressed: printLog,
+            ),
+          ],
         ),
       ),
     );
